8e3ee9bac60f7e2a6a33e8437ea47e5aa89b4aaa
Gabriel Denita <gabriel.denita@gmail.com>
2023-07-05 00:59:40 -0300

    Se crea el archivo product.json y se agregan las funciones updaProduct y deletedProduct

--- desafio02.js
+++ desafio02.js
@@ -1,28 +1,34 @@
 const fs = require('fs/promises');
 const path = require('path');
-
-
+const filename ='product.json'
 
 class ProductManager{
-    constructor(){
+    constructor(path){
         this.product=[]
-        this.filepath = path
+        this.filepath = path//.join(__dirname, filename)
     }
     
-    async addProduct({title, description, price, thumbnail, code, stock}){   
+    async addProduct({title, description, price, thumbnail, code, stock}){
+           
         const data = await fs.readFile(this.filepath, 'utf-8');
         const product = JSON.parse(data);
-
+        
         if(title && description && price && thumbnail && code && stock){
             const newId = product[product.length - 1]?.id || 0
-            const producto = this.product.find(e => e.code === code)
+            const producto = product.find(e => e.code === code)
             if (producto){
                 return console.log("el codigo es invalido")
             }else if (!producto){
-                this.product.push({
-                    ...producto,
-                    id: newId + 1
+                product.push({
+                    title,
+                    description,
+                    price,
+                    thumbnail,
+                    code,
+                    stock,
+                    id: newId + 1,
                 })
+                console.log(product)
                 console.log ("el producto se agregó exitosamente")
                 
             }
@@ -54,34 +60,58 @@ class ProductManager{
             console.log(producto)
         }
     }
+    async updateProduct(idBuscado, codeNuevo){
+        const data= await fs.readFile(this.filepath, 'utf-8')
+        const product = JSON.parse(data)
+        let encontrado = product.find(e => e.id ===idBuscado)
+        if (!encontrado){
+            return console.log("el id es invalido")
+        }
+        if (encontrado){
+            encontrado.code = codeNuevo
+            console.log(encontrado)
+        }
+        await fs.writeFile(this.filepath, JSON.stringify(product, null, 2))
+    }
     
+    async deletedProduct(idBuscado) {
+        const data= await fs.readFile(this.filepath, 'utf-8')
+        const product = JSON.parse(data)
+        const productoFiltrado= product.filter(e=>e.id!==idBuscado)
+            console.log(productoFiltrado)
+        
+        await fs.writeFile(this.filepath, JSON.stringify(productoFiltrado, null, 2))
+    }
+
+
 }
 
-const productoNuevo =new ProductManager(path.join(__dirname, 'product.JSON'))
+const productoNuevo =new ProductManager(path.join(__dirname, filename))
 
 async function main(){
-    console.log(await productoNuevo.getProduct())
-
+    
     await productoNuevo.addProduct({ 
-        title: 'aaaaaa',
+        title: "5555aa",
         description: "cuatro puertas",
         price: "100",
         thumbnail: "dasfasdadsa",
-        code: "ABC1",
+        code: "ABC4",
         stock: 20
     })
-    console.log(await productoNuevo.getProduct)
-
+    
 }
 
-main()
+//main()
 
-//  productoNuevo.addProduct({title: 'aaaaaa', description: "cuatro puertas", price: "100", thumbnail: "dasfasdadsa", code: "ABC1", stock: 20})
-//  productoNuevo.addProduct({ title: 'asdfasd', description: "aadsfasdasszxcvz", price: "10", thumbnail: "asdfasd", code: "ABC2" , stock: 55})
-//  productoNuevo.addProduct({ title: 'auto', description: "3 puertas", price: "500", thumbnail: "dasfasdadsa", code: "ABC6", stock: 5})
-//  productoNuevo.addProduct({ title: 'moto', description: "0 puertas", price: "500", thumbnail: "dasfasdadsa", code: "ABC3", stock: 5})
-//  productoNuevo.addProduct({ title: 'camioneta', description: "3 puertas", price: "500", thumbnail: "dasfasdadsa", code: "ABC4", stock: 5})
+// productoNuevo.addProduct({
+//     title: "aaaaaa",
+//     description: "cuatro puertas",
+//     price: "100",
+//     thumbnail: "dasfasdadsa",
+//     code: "ABC7",
+//     stock: 20
+// })
+// console.log(productoNuevo.getProduct)
 
-
-// console.log(p.getProduct())
-// // console.log(p.getProductById(3))
+//productoNuevo.updateProduct(2, "ERT5")
+productoNuevo.deletedProduct(2)
